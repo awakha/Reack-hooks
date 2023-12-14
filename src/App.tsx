1352dbb021c8d976062bd9e224c49e2ddcc4cad0
awakha <pagalova97@mail.ru>
2023-12-14 21:14:57 +0300

    part

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import Form from "./components/form/Form";
+
 import "./App.css";
 
 function App(): JSX.Element {
   //                                джинерики
   const [count, setCount] = useState<number>(0);
-  const increment = () => {
+  //              функция не возвращает никакого значения
+  const increment = (): void => {
     setCount((prev) => prev + 1);
   };
-  const decrement = () => {
+  const decrement = (): void => {
     setCount((prev) => prev - 1);
   };
 
@@ -16,6 +19,7 @@ function App(): JSX.Element {
       <button onClick={increment}>+</button>
       <span>{count}</span>
       <button onClick={decrement}>-</button>
+      <Form />
     </>
   );
 }
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,43 +1,5 @@
-import { type ChangeEvent, type MouseEvent, useState, type Dispatch, SetStateAction } from 'react'
-import axios from 'axios';
-import type { TodosType } from '../../App';
+import React from "react";
 
-type InputType = {
-  title: string;
-  text: string;
-}
-
-type FormPropsType = {
-  setTodos: Dispatch<SetStateAction<TodosType>>
-}
-
-export default function Form({ setTodos }: FormPropsType): JSX.Element {
-  const [inputs, setInputs] = useState<InputType>({ title: '', text: '' })
-
-  const chengeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    // console.log(e.target.name, e.target.value);
-    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-  }
-
-  const addHendler = async (e: MouseEvent<HTMLButtonElement>): Promise<void> => {
-    console.log(e.target);
-    console.log('inputs', inputs);
-    try {
-      const response = await axios.post('http://localhost:3100/api', inputs)
-      if (response.status === 200) {
-        setTodos((prev) => ([...prev, response.data]))
-        setInputs({ title: '', text: '' })
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  return (
-    <form>
-      <input onChange={chengeHandler} type="text" name="title" placeholder="title" value={inputs.title} />
-      <input onChange={chengeHandler} type="text" name="text" placeholder="text" value={inputs.text} />
-      <button onClick={addHendler} type="button">Send</button>
-    </form>
-  )
+export default function Form(): JSX.Element {
+  return <div>Form</div>;
 }
