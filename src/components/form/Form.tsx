275c2c77dc125296cde27ede3c55b012c5fbf819
awakha <pagalova97@mail.ru>
2023-12-15 10:10:20 +0300

    comments in App.tsx

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Form(): JSX.Element {
-  return <div>Form</div>;
+  const [inputs, setInputs] = useState({ title: "", text: "" });
+
+  const changeHandler = (e: any) => {
+    console.log(e.target.name, e.target.value);
+  };
+
+  return (
+    <form>
+      <input
+        onChange={changeHandler}
+        type="text"
+        name="title"
+        placeholder="title"
+      />
+      <input
+        onChange={changeHandler}
+        type="text"
+        name="text"
+        placeholder="text"
+      />
+      <button type="button">Send</button>
+    </form>
+  );
 }
